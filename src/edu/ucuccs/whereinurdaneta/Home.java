4e6de65e5656d853c0b74ae9bed7041343918170
gebil <garibaygeno30@gmail.com>
2014-10-09 01:33:04 -0700

    4th commit

--- src/edu/ucuccs/whereinurdaneta/Home.java
+++ src/edu/ucuccs/whereinurdaneta/Home.java
@@ -1,76 +1,126 @@
 package edu.ucuccs.whereinurdaneta;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class Home extends Activity {
 
-	ImageView img_universities;
-	ImageView img_recreational;
-	ImageView img_restaurant;
-	ImageView img_terminal;
-	ImageView img_hotel;
-	ImageView img_mall;
+	// Array of strings storing country names
+	String[] name_place = new String[] { "Colleges & Universities", "Malls",
+			"Hotels", "Terminals", "Restuarants", "Recreational Area" };
+
+	// Array of integers points to images stored in /res/drawable-ldpi/
+	int[] img = new int[] { R.drawable.ic_uni, R.drawable.ic_shop,
+			R.drawable.ic_restau, R.drawable.ic_bu, R.drawable.ic_hote,
+			R.drawable.ic_recrea };
+
+	// flag for Internet connection status
+	Boolean isInternetPresent = false;
+
+	// Connection detector class
+	InternetConnection cd;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		setContentView(R.layout.activity_home);
 
-		img_universities = (ImageView) findViewById(R.id.imageButton2);
-		img_recreational = (ImageView) findViewById(R.id.imageButton3);
-		img_restaurant = (ImageView) findViewById(R.id.imageButton4);
-		img_terminal = (ImageView) findViewById(R.id.imageButton5);
-		img_hotel = (ImageView) findViewById(R.id.imageButton6);
-		img_mall = (ImageView) findViewById(R.id.imageButton7);
+		// creating connection detector class instance
+		cd = new InternetConnection(getApplicationContext());
+
+		// Getting a reference to listview of main.xml layout file
+		ListView listView = (ListView) findViewById(R.id.list);
+
+		List<HashMap<String, Object>> data_list = new ArrayList<HashMap<String, Object>>();
+
+		for (int i = 0; i < 6; i++) {
+			HashMap<String, Object> hm = new HashMap<String, Object>();
+			hm.put("placeName", name_place[i]);
+			hm.put("place_image", Integer.toString(img[i]));
+			data_list.add(hm);
+
+		}
+
+		// Instantiating an adapter to store each items
+		SimpleAdapter adapter = new SimpleAdapter(this, data_list,
+				R.layout.listview_layout, new String[] { "place_image",
+						"placeName" }, new int[] { R.id.place_image,
+						R.id.placeName });
+
+		// Setting the adapter to the listView
+		listView.setAdapter(adapter);
+
+		// get Internet status
+		isInternetPresent = cd.isConnectingToInternet();
+		// check for Internet status
+		if (isInternetPresent) {
+			// Internet Connection is Present
+			// make HTTP requests
+
+			listView.setOnItemClickListener(new OnItemClickListener() {
+
+				@Override
+				public void onItemClick(AdapterView<?> arg0, View arg1,
+						int arg2, long arg3) {
+					// TODO Auto-generated method stub
+
+					
+						Intent intent = new Intent(Home.this,
+								WhereInUrdaneta.class);
+						
+
+						// Start SingleItemView Class
+						startActivity(intent);
+						
+
+				}
+
+			});
+
+		} else {
+			// Internet connection is not present
+			// Ask user to connect to Internet
+			showAlertDialog(Home.this, "No Internet Connection",
+					"You don't have internet connection.", false);
+		}
 
 	}
 
-	public void img_terminal(View v) {
+	@SuppressWarnings("deprecation")
+	public void showAlertDialog(Context context, String title, String message,
+			Boolean status) {
+		AlertDialog alertDialog = new AlertDialog.Builder(context).create();
 
-		// TODO Auto-generated method stub
-		Intent terminal = new Intent(Home.this, WhereInUrdaneta.class);
-		startActivity(terminal);
+		// Setting Dialog Title
+		alertDialog.setTitle(title);
 
-	}
+		// Setting Dialog Message
+		alertDialog.setMessage(message);
 
-	public void img_universities(View v) {
-		// TODO Auto-generated method stub
-		Intent uni = new Intent(Home.this, WhereInUrdaneta.class);
-		startActivity(uni);
+		// Setting alert dialog icon
+		alertDialog.setIcon(R.drawable.alert);
 
-	}
+		// Setting OK Button
+		alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+			}
+		});
 
-	public void img_recreational(View v) {
-		// TODO Auto-generated method stub
-		Intent recreational = new Intent(Home.this, WhereInUrdaneta.class);
-		startActivity(recreational);
-
-	}
-
-	public void img_restaurant(View v) {
-		// TODO Auto-generated method stub
-		Intent restaurant = new Intent(Home.this, WhereInUrdaneta.class);
-		startActivity(restaurant);
-
-	}
-
-	public void img_hotel(View v) {
-		// TODO Auto-generated method stub
-		Intent hotel = new Intent(Home.this, WhereInUrdaneta.class);
-		startActivity(hotel);
-
-	}
-
-	public void img_mall(View v) {
-		// TODO Auto-generated method stub
-		Intent mall = new Intent(Home.this, WhereInUrdaneta.class);
-		startActivity(mall);
-
+		// Showing Alert Message
+		alertDialog.show();
 	}
 
 }
--- src/edu/ucuccs/whereinurdaneta/WhereInUrdaneta.java
+++ src/edu/ucuccs/whereinurdaneta/WhereInUrdaneta.java
@@ -1,42 +1,40 @@
 package edu.ucuccs.whereinurdaneta;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 public class WhereInUrdaneta extends Activity {
 
-	EditText editText;
-	ImageView img_btn;
 	RelativeLayout rel;
 	private GoogleMap GMap;
+
 	private static final LatLng URDANETA = new LatLng(15.9751895, 120.5703162);
 	ParseObject p;
 	final String APPLICATION_ID = "7BVKmFj5lddiYXKIUHSRWwTh8AbilJUbf0L5iPxX";
 	final String CLIENT_KEY = "XAessF2ggXY69PXz8aeyD2uptaKi8YohToG3ePS5";
 
-	LatLng PSU = new LatLng(15.9782426, 120.5715607);
-	LatLng PUNP = new LatLng(15.9847439, 120.5726536);
-	LatLng ABE = new LatLng(15.9826905, 120.5714945);
-	LatLng UCU = new LatLng(15.980504, 120.560681);
-	LatLng Manantan = new LatLng(15.980216, 120.5717094);
-
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_where_in_urdaneta);
-
-		editText = (EditText) findViewById(R.id.editText);
-		img_btn = (ImageView) findViewById(R.id.imgButton);
 		rel = (RelativeLayout) findViewById(R.id.layoutbelow);
 		rel.setVisibility(View.VISIBLE);
 
@@ -49,16 +47,82 @@ public class WhereInUrdaneta extends Activity {
 
 		GMap.moveCamera(CameraUpdateFactory.newLatLng(URDANETA));
 		GMap.animateCamera(CameraUpdateFactory.zoomIn());
-		GMap.animateCamera(CameraUpdateFactory.zoomTo(14), 2000, null);
+		GMap.animateCamera(CameraUpdateFactory.zoomTo(13), 2000, null);
 
-	}
+		Parse.initialize(this, APPLICATION_ID, CLIENT_KEY);
 
-	public void click_search(View v) {
-		ParseObject p = new ParseObject("Locations");
-		ParseGeoPoint geo_point = new ParseGeoPoint();
-		p.put("location", geo_point);
-		p.saveInBackground();
+		ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Locations");
 
+		query.findInBackground(new FindCallback<ParseObject>() {
+
+			@Override
+			public void done(List<ParseObject> listahan, ParseException e) {
+				// TODO Auto-generated method stub
+
+				List<Marker> markers = new ArrayList<Marker>();
+
+				for (int i = 0; i < listahan.size(); i++) {
+
+					listahan.get(i).getParseGeoPoint("location");
+
+					ParseGeoPoint geo = listahan.get(i).getParseGeoPoint(
+							"location");
+					Marker marker = GMap.addMarker(new MarkerOptions()
+							.position(
+									new LatLng(geo.getLatitude(), geo
+											.getLongitude()))
+							.icon(BitmapDescriptorFactory
+									.fromResource(R.drawable.university))
+
+					);
+
+					markers.add(marker);
+			
+				}
+				
+			}
+
+		});
+		GMap.clear();
+		
+	
+
+		ParseQuery<ParseObject> malls = new ParseQuery<ParseObject>("Malls");
+
+		malls.findInBackground(new FindCallback<ParseObject>() {
+
+			@Override
+			public void done(List<ParseObject> listahan, ParseException e) {
+				// TODO Auto-generated method stub
+
+				List<Marker> markers = new ArrayList<Marker>();
+
+				for (int i = 0; i < listahan.size(); i++) {
+
+					listahan.get(i).getParseGeoPoint("LatLang");
+
+					ParseGeoPoint geo = listahan.get(i).getParseGeoPoint(
+							"LatLang");
+
+					Marker marker = GMap.addMarker(new MarkerOptions()
+							.position(
+									new LatLng(geo.getLatitude(), geo
+											.getLongitude())).icon(
+									BitmapDescriptorFactory
+											.fromResource(R.drawable.mall))
+
+					);
+					
+					markers.add(marker);
+
+				}
+			
+			}
+
+		});
+		GMap.clear();
+		GMap.clear();
+		
 	}
 
 }
